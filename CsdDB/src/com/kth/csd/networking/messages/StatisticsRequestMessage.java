8cb9011c33201f3529a482aba69232622ca61373
Argy <anargyros.chatzaras@apegroup.com>
2014-12-13 13:01:45 +0100

    added serailizable VersionId

--- CsdDB/src/com/kth/csd/networking/messages/StatisticsRequestMessage.java
+++ CsdDB/src/com/kth/csd/networking/messages/StatisticsRequestMessage.java
@@ -6,6 +6,8 @@ import com.kth.csd.networking.messages.AbstractNetworkMessage.type;
 
 public class StatisticsRequestMessage extends AbstractNetworkMessage{
 	
+	private static final long serialVersionUID = 5531626847933761299L;
+
 	public StatisticsRequestMessage(ArrayList<String> listOfYcsbClients){
 		super(type.STATISTICS_REQ);
 		mData = new ArrayList<String>(listOfYcsbClients);
