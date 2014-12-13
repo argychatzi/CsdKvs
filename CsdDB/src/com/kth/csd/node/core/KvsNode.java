7dc88cf4fde555e1ca9b7e0fd4bfddd1e0f8e16c
Argy <anargyros.chatzaras@apegroup.com>
2014-12-13 13:03:16 +0100

    removed commented code, small refactoring on sending statistics requests

--- CsdDB/src/com/kth/csd/node/core/KvsNode.java
+++ CsdDB/src/com/kth/csd/node/core/KvsNode.java
@@ -28,10 +28,6 @@ public class KvsNode {
 
 	protected static final String TAG = KvsNode.class.getCanonicalName();
 //	private static ArrayList<ConnectionMetaData> allNodeProperties = new ArrayList<ConnectionMetaData>();
-	//TODO remove hardcoded values
-	public static String[] allNodeIp = { "192.168.0.2", "192.168.0.3", "192.168.0.4", "192.168.0.5", "192.168.0.7","192.168.0.8",
-		"192.168.0.10","192.168.0.11","192.168.0.12","192.168.0.13" };
-	//TODO remove hardcoded values
 	public static String[] allYCSBIp = { "192.168.0.1", "192.168.0.6","192.168.0.9" };// for testing
 //	private static AbstractNetworkMessage requestMsg;
 //	private static AbstractNetworkMessage resultsMsg;
@@ -58,22 +54,18 @@ public class KvsNode {
         	ApplicationContext.setOwnExternalConnection(configuration.getOwnExternalConnectionMetaData());
         	ApplicationContext.setOwnInternalConnection(configuration.getOwnInternalConnectionMetaData());
     		
-        	startMonitoringKvsSocket(new ServerInternalInputInterface(), ApplicationContext.getInternalConnection().getPort());
-        	startMonitoringKvsSocket(new ServerExternalInputInterface(), ApplicationContext.getExternalConnection().getPort());
+        	startMonitoringKvsSocket(new ServerInternalInputInterface(), ApplicationContext.getOwnInternalConnection().getPort());
+        	startMonitoringKvsSocket(new ServerExternalInputInterface(), ApplicationContext.getOwnExternalConnection().getPort());
         	
         	ApplicationContext.generateNodeFarm(configuration.getNodesInFarm());
+        	
+        	startPollingFarmForStatistics();
     	}
-
-		if (ApplicationContext.isMaster()){	
-			Logger.d(TAG, "I am a master");
-		}
-		else
-			Logger.d(TAG, "I am a slave");
-			
-
-		if(ApplicationContext.isMaster()){
-			System.out.println("I am the master");
-			System.out.println("Sending req for stats");
+    }
+    
+    private static void startPollingFarmForStatistics(){
+    	//TODO this function makes sense to be run only and when there is a ycsb interaction in place
+    	if(ApplicationContext.isMaster()) {
 			Timer timer = new Timer();
 			TimerTask task = new TimerTask(){
 				//For every second, broadcast a request
@@ -82,53 +74,23 @@ public class KvsNode {
 					//Broadcast to a nodeFarm. 
 					//You put the IP and the port of slaves you want to connect into myArray
 					//Then generate a NodeFarm
-					myArray = new ArrayList<ConnectionMetaData>();
-					myArray.add(new ConnectionMetaData(allNodeIp[1],ApplicationContext.getInternalConnection().getPort()));
-					
-					ApplicationContext.generateNodeFarm(myArray);
-					//The message will tell the slaves, please ping the clients
-					//So, the listOfYcsbClient should be a part of the message		
-					ArrayList<String> listOfYcsbClients = new ArrayList<String>();
-					for(int i =0;i<allYCSBIp.length;i++){
-						listOfYcsbClients.add(allYCSBIp[i]);
-					}
-					ApplicationContext.setYcsbIPs(listOfYcsbClients);		
+
+					ArrayList<String> listOfYcsbClients = ApplicationContext.getYcsbIPs();
 					AbstractNetworkMessage requestMsg = new StatisticsRequestMessage(listOfYcsbClients);		
 					Logger.d(TAG, "requestMsg = " +  requestMsg.toString() );
-					
 					//Broadcast
-					ApplicationContext.getNodes().broadCast(requestMsg);
+					ApplicationContext.getNodeFarm().broadCast(requestMsg);
 					Logger.d(TAG, "Broadcast finished" );
 				}
-				
 			};
 			timer.scheduleAtFixedRate(task, 0, sendRequestInterval);	
 		}
-		
+    }
 
-//		HashMap<String, Integer> results = new HashMap<String, Integer>();
-//		results.put("resultsRequest", 1);
-//		AbstractNetworkMessage resultsMsg = new StatisticsResultMessage(results);
-//		System.out.println("resultsMsg =  " +resultsMsg);
-//		ApplicationContext.getNodes().broadCast(resultsMsg);
-//		System.out.println("*******Message sent on broadcast*******");
-//		System.out.println("Listening for incoming connections as master");
-
-	}
-
-//	public static String[] allNodeIp = { "192.168.0.2", "192.168.0.3", "192.168.0.4", "192.168.0.5", "192.168.0.7","192.168.0.8",
-//		"192.168.0.10","192.168.0.11","192.168.0.12","192.168.0.13" };
-	//public static String[] allNodeIp = { "10.0.0.1", "10.0.0.3" };// for testing
-		
-    
     private static Configuration parseConfigurationFile(String fileNo) throws IOException {
-    	
-    	Logger.d(TAG, fileNo);
     	Configuration configuration = ConfigurationReader.loadConfigurationFile(fileNo);
-    	
     	Logger.d(TAG, "configuration :: " + configuration);
     	return configuration; 
-    	
 	}
 
     private static void startMonitoringKvsSocket(IoHandler handler, final int portNumber) throws IOException {
--- CsdDB/src/com/kth/csd/node/core/NodeFarm.java
+++ CsdDB/src/com/kth/csd/node/core/NodeFarm.java
@@ -5,27 +5,18 @@ import java.util.ArrayList;
 import com.kth.csd.networking.ConnectionMetaData;
 import com.kth.csd.networking.KvsClient;
 import com.kth.csd.networking.interfaces.internal.ClientInternalInputInterface;
-import com.kth.csd.networking.interfaces.internal.ServerInternalInputInterface;
 import com.kth.csd.networking.messages.AbstractNetworkMessage;
-import com.kth.csd.utils.Logger;
 
 public class NodeFarm {
-
-	//NodeFarm is a list of many connections
 	private static ArrayList<KvsClient> mNodeFarm;
 	private static final String TAG = NodeFarm.class.getCanonicalName();
 
 	public NodeFarm(ArrayList<ConnectionMetaData> nodeIps) {
 		mNodeFarm = new ArrayList<KvsClient>();
-		Logger.d(TAG,  "size "+nodeIps.size());
 
 		for(ConnectionMetaData connectionMetaData: nodeIps){
-			Logger.d(TAG, " connectionMetaData " + connectionMetaData.toString());
-			Logger.d(TAG, " ApplicationContext.getMasterInternalConnection()" + ApplicationContext.getMasterInternalConnection());
-			if(!connectionMetaData.equals(ApplicationContext.getMasterInternalConnection())){
-				Logger.d(TAG,  "connectionMetaData " + connectionMetaData.toString());
+			if(!connectionMetaData.equals(ApplicationContext.getOwnInternalConnection())){
 				KvsClient newOne = new KvsClient(new ClientInternalInputInterface(), connectionMetaData);
-				Logger.d(TAG,  "Return ");
 				mNodeFarm.add(newOne);
 			}
 		}
@@ -34,6 +25,7 @@ public class NodeFarm {
 	public void broadCast(AbstractNetworkMessage message){
 		System.out.print("mNodeFarm="+mNodeFarm==null);
 		for(KvsClient node: mNodeFarm){
+			//TODO check if master should be receiving braodcast messages!
 			System.out.print("message="+message);
 			System.out.print("node="+node);
 			node.send(message);
